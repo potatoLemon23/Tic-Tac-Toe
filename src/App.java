0f6f42019845cc6978af9212350b9bf90cc21702
potatoLemon23 <coralreader@lords-house.net>
2022-12-02 09:27:54 -0600

    Update

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        System.out.println("Would you like to play tic tac toe?");
+
     }
 }
